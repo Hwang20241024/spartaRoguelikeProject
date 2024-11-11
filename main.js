db0d7089c0dbbcd1be1be7b18d3638c6c2ddd5c7
Hwang20241024 <ghkdaksdbs@naver.com>
2024-11-11 12:44:36 +0900

    이름 변경

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-// chalk figlet readline-sync inquirer 라이브러리 추가.
+// 최상위 main.js : chalk figlet readline-sync inquirer 라이브러리 추가.
 import chalk from 'chalk';
 import figlet from 'figlet';
 import readlineSync from 'readline-sync';
 import inquirer from 'inquirer';
 
 // 이곳엔 스크립트 추가
-import { SceneManager } from './scripts/sceneManager.js';
+import { SceneManager } from './scripts/SceneManager.js';
 
 
 // 이곳에서 메인 로직을 작성 합니다.
