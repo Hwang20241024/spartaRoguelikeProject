7e9d02585f33f234456390dad273f5c64eef1f01
Hwang20241024 <ghkdaksdbs@naver.com>
2024-11-11 11:57:14 +0900

    라이브러리 setting
    chalk figlet readline-sync inquirer 라이브러리 추가

---
+++ main.js
@@ -0,0 +1,8 @@
+// chalk figlet readline-sync inquirer 라이브러리 추가.
+import chalk from 'chalk';
+import figlet from 'figlet';
+import readlineSync from 'readline-sync';
+import inquirer from 'inquirer';
+
+
+// 이곳에서 메인 로직을 작성 합니다.
